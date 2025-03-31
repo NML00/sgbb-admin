f5e2a66a45cd7383498778ca0b717fbe58414e4e
NML00 <NML00@users.noreply.github.com>
2025-03-31 15:42:20 +0000

    deploy: 9d57a2fa0da1a7117e3521657d94d307327d9d99

--- assets/Index-Be3TOjjr.js
+++
@@ -1,2 +0,0 @@
-import{m as c,q as r,F as s,C as _,G as l,I as d}from"./index-Wee15PfQ.js";const p=(t,n)=>{const e=t.__vccOpts||t;for(const[o,a]of n)e[o]=a;return e},f={};function m(t,n){const e=d("page-header");return r(),c("div",null,[s(e,null,{default:_(()=>[l("Settings")]),_:1})])}const i=p(f,[["render",m]]);export{i as default};
-//# sourceMappingURL=Index-Be3TOjjr.js.map
