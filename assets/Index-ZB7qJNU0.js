6d1654f7525b347a7f7c9821618be272eb78dbdd
NML00 <NML00@users.noreply.github.com>
2025-03-31 16:28:58 +0000

    deploy: 59f6205ed0798de63ea48fbf6a6a14e50380634e

---
+++ assets/Index-ZB7qJNU0.js
@@ -0,0 +1,2 @@
+import{m as c,q as r,F as s,C as _,G as l,I as d}from"./index-DqO8xdVG.js";const p=(t,n)=>{const e=t.__vccOpts||t;for(const[o,a]of n)e[o]=a;return e},f={};function m(t,n){const e=d("page-header");return r(),c("div",null,[s(e,null,{default:_(()=>[l("Settings")]),_:1})])}const i=p(f,[["render",m]]);export{i as default};
+//# sourceMappingURL=Index-ZB7qJNU0.js.map
