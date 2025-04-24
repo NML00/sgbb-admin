2424b0cebd9cddb73c6238185b0d3b73ca31d0fe
NML00 <NML00@users.noreply.github.com>
2025-04-24 08:41:39 +0000

    deploy: d37ed29cf67796e37185ace5606e0034cab8e099

---
+++ assets/AvatarFallback.vue_vue_type_script_setup_true_lang-DW5ylLHi.js
@@ -0,0 +1,2 @@
+import{d as o,p as r,o as t,w as n,R as p,Z as l,$ as c,e as d,a0 as i}from"./index-De3nUd0-.js";const f=o({__name:"AvatarFallback",props:{delayMs:{},asChild:{type:Boolean},as:{}},setup(a){const e=a;return(s,u)=>(t(),r(d(i),l(c(e)),{default:n(()=>[p(s.$slots,"default")]),_:3},16))}});export{f as _};
+//# sourceMappingURL=AvatarFallback.vue_vue_type_script_setup_true_lang-DW5ylLHi.js.map
